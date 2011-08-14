4eb0b168d149ec1873724b3f53ceade540815b84
Justin Rolston <justinrolston@gmail.com>
2011-08-13 22:21:56 -0400

    added body corrected

--- db/migrate/20110814014327_create_messages.rb
+++ db/migrate/20110814014327_create_messages.rb
@@ -4,7 +4,7 @@ class CreateMessages < ActiveRecord::Migration
       t.integer :from
       t.string :to
       t.string :subject
-      t.string :body
+      t.text :body
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110814010221) do
+ActiveRecord::Schema.define(:version => 20110814014327) do
 
   create_table "messages", :force => true do |t|
     t.integer  "from"
     t.string   "to"
     t.string   "subject"
-    t.string   "body"
+    t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
