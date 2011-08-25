a1eab3800844c2a6a6be6ecf59cf2106b5298b5a
Justin Rolston <justinrolston@gmail.com>
2011-08-24 20:22:25 -0400

    Added Group Mailer

---
+++ app/mailers/notifier.rb
@@ -0,0 +1,15 @@
+class Notifier < ActionMailer::Base
+  default :from => "justinrolston@gmail.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.notifier.send_group_message.subject
+  #
+  def send_group_message(msg)
+    @message = msg
+
+    mail :to => msg.to, :subject => msg.subject
+
+  end
+end
